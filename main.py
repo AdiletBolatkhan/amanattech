cb36388ae1015fce515e7d6cf4380c42aebbb71d
abolatkh <adilet.bolatkhan@nu.edu.kz>
2023-11-27 17:01:16 +0600

    two account send

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 from telethon import TelegramClient, events
+from telethon.errors import SessionPasswordNeededError
+import asyncio
 
-api_id = '20865934'  # Replace with your api_id
-api_hash = '26f2be56f24fffb631180d7c07b92ea4'  # Replace with your api_hash
-phone_number = '+77025344128'  # Replace with your phone number
+# First account
+api_id_1 = '20865934'
+api_hash_1 = '26f2be56f24fffb631180d7c07b92ea4'
+phone_number_1 = '+77025344128'
+client1 = TelegramClient('session_name_1', api_id_1, api_hash_1)
 
-client = TelegramClient('session_name', api_id, api_hash)
+# Second account
+api_id_2 = '25525581'
+api_hash_2 = '8264205a15a7424a32e2054806d5f734'
+phone_number_2 = '+77763855554'
+client2 = TelegramClient('session_name_2', api_id_2, api_hash_2)
 
-async def main():
+async def authorize_client(client, phone_number):
     await client.start(phone=phone_number)
     print("Client Created")
-    # Ensure you're authorized
-    if await client.is_user_authorized() == False:
+    if not await client.is_user_authorized():
         await client.send_code_request(phone_number)
         try:
-            await client.sign_in(phone_number, input('Enter the code: '))
+            await client.sign_in(phone_number, input('Enter the code for ' + phone_number + ': '))
         except SessionPasswordNeededError:
-            await client.sign_in(password=input('Password: '))
+            await client.sign_in(password=input('Password for ' + phone_number + ': '))
 
-    # Now you can use client to send messages, interact, etc.
+async def send_message_from_client1():
+    await authorize_client(client1, phone_number_1)
+    await asyncio.sleep(5)  # Wait for 5 seconds before sending the message
+    await client1.send_message('bushpulbek', 'Hello! This is a testerf message.')
 
-with client:
-    client.loop.run_until_complete(main())
-async def send_message():
-    await client.send_message('bushpulbek', 'Hello! This is a testerf message.')
-    
-with client:
-    client.loop.run_until_complete(send_message())
+async def send_message_from_client2():
+    await authorize_client(client2, phone_number_2)
+    await client2.send_message('bushpulbek', 'heyyy')
+
+async def main():
+    await asyncio.gather(
+        send_message_from_client1(),
+        send_message_from_client2()
+    )
+
+with client1, client2:
+    client1.loop.run_until_complete(main())
